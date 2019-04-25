914460154a6972fccd71e10d32ebca37c9d9ee71
ThyMarX <fake.dk@hotmail.com>
2019-04-25 14:22:22 +0200

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,22 +1,7 @@
 // JavaScript Document
 
-/*   ---1. banner som link---   */
-
-let element = document.querySelector(".banner");
-
-element.addEventListener("click", goToSomePage);
-
-function goToSomePage(){
-	window.open("http://thomasdsiemsen.com", '_blank');
-}
-
-
-/*   ---2. banner auto refresh---   */
-
 document.addEventListener("load", setTimeout(reloadBanner, 10000));
 
 function reloadBanner(){
 	location.reload();
 }
-
-
